f0dd026ee73d74dd13f7ece2d3f48544c62392e3
ZGMauri <mauri1891.mm@gmail.com>
2022-06-30 17:21:36 -0300

    carrito de compras

--- js/archivo.js
+++ js/archivo.js
@@ -1,18 +1,11 @@
 //Variables y funciones
-let resultado = 8;
+/*let resultado = 8;
 let meses = 10;
 let nombre;
 let cuota;
 
 function sumar (numerouno, numerodos) {
     return numerouno * numerodos
-}
-
-function CambioDePrecio (APrecio) {
-    for(let i=0; i<APrecio.lenght; i++) {
-        let cambio = [i];
-        cambio.innerText = "20 USDT";
-      }
 }
 
 //Declaracion de clases y objetos
@@ -48,7 +41,7 @@ vender () {
 
 //Arreglos
 let arrayPersona = [];
-var arrayProducto = [];
+let arrayProducto = [];
 
 //Verificando si el cliente es socio y posee un NFT del sitio
 let socio = prompt ("¿Ya sos socio? S/N").toUpperCase();
@@ -84,7 +77,7 @@ while (socio =="N"){
 } 
 
 alert ("¡GRACIAS POR ELEGIRNOS!");
-var controlNFT=prompt("¿Sabias que ademas podes acceder a beneficios exclusivos con tu NFT? Presiona 0 si ya tenes el tuyo, 1 si te gustaria adquirir uno o 2 para salir.");
+let controlNFT=prompt("¿Sabias que ademas podes acceder a beneficios exclusivos con tu NFT? Presiona 0 si ya tenes el tuyo, 1 si te gustaria adquirir uno o 2 para salir.");
 switch (controlNFT) {
     case "0":
         arrayPersona[0].NFT=true;
@@ -100,21 +93,21 @@ alert(JSON.stringify(arrayPersona));
 
 //Agregando productos al carrito
 do {
-    var seleccionar = prompt("Indique el nombre del producto que desea comprar o ESC para salir").toUpperCase();
+    let seleccionar = prompt("Indique el nombre del producto que desea comprar o ESC para salir").toUpperCase();
     if (seleccionar==="ESC"){
         break;
     }
     else{
-        var nombreP=seleccionar;
-        var cantidadP=prompt("Ingrese la cantidad que quiere comprar");
-        var precioP=10;
+        let nombreP=seleccionar;
+        let cantidadP=prompt("Ingrese la cantidad que quiere comprar");
+        let precioP=10;
         arrayProducto.push(new Producto(nombreP,precioP,cantidadP));
     }
 }while (seleccionar!="ESC")
 
 console.log(arrayProducto);
 
-for (var producto of arrayProducto){
+for (let producto of arrayProducto){
     document.write ("<ul><li>Nombre:" + producto.nombre + "</li>");
     document.write ("<li>Cantidad:" + producto.cantidad + "</li>");
     document.write ("<li>Precio:" + producto.precio + "</li>");
@@ -139,30 +132,154 @@ for (var producto of arrayProducto){
     }
 }
 
-//AGREGANDO PRODUCTOS SECCION MERCHANDISING
-const NuevosProductos = [
-    { nombre:"Remera Solana", descripcion: "Remera perzonalizada Solana", precio: 15},
-    { nombre:"Remera Litecoin", descripcion: "Remera perzonalizada Litecoin", precio: 15},
-    { nombre:"Remera BNB", descripcion: "Remera perzonalizada Binance", precio: 15}
-];
+*/
+//CARRITO DE COMPRAS -- REMERAS
+const cards = document.getElementById('cards')
+const items = document.getElementById('items')
+const footer = document.getElementById('footer')
 
-for (const Prod of NuevosProductos) {
-    let NuevaRemera = document.createElement("div");
-    NuevaRemera.innerHTML = `<h5> ${Prod.nombre} </h5>
-                            <p> ${Prod.descripcion} </p>
-                            <a> ${Prod.precio} </a>`;
-    document.container.appendChild(NuevaRemera);
+const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
+
+const fragment = document.createDocumentFragment()
+let carrito = {}
+
+document.addEventListener('DOMContentLoaded', () => { fetchData() })
+
+cards.addEventListener('click', e => addCarrito(e))
+
+//items.addEventListener('click', e => accionBotones())
+
+
+const pintarCards = producto => {
+	
+	for (const key in producto) {
+	    const element = producto[key];
+	    
+	    templateCard.querySelector('h5').textContent = element.nombre
+	    templateCard.querySelector('p').textContent = element.precio
+	    templateCard.querySelector('img').setAttribute("src", element.img)
+	    templateCard.querySelector('.btn-dark').dataset.id = key
+
+	    const clone = templateCard.cloneNode(true)
+	    fragment.appendChild(clone)
+	}	
+	cards.appendChild(fragment)
 }
 
-//MODIFICANDO PRECIOS LLAMANDO A UNA FUNCION    
-const ArregloPreciosOLD = [document.querySelector(".container a")];
-const ArregloPreciosNEW = [];
-ArregloPreciosNEW = CambioDePrecio(ArregloPreciosOLD);
-console.log(ArregloPreciosNEW);
+const addCarrito = e => {
+	if(e.target.classList.contains('btn-dark')) {
+		setCarrito(e.target.parentElement)
+	}
+	e.stopPropagation()
+}
 
-//EL EVENTO ESTA PROGRAMADO COMO UN SCRIPT EN MERCHANDISING.HTML
+const setCarrito = objeto => {
+	const producto = {
+		id: objeto.querySelector('.btn-dark').dataset.id,
+		nombre: objeto.querySelector('h5').textContent, 
+		precio: objeto.querySelector('p').textContent,
+		cantidad: 1
+	}
+
+	if(carrito.hasOwnProperty(producto.id)) {
+		producto.cantidad = carrito[producto.id].cantidad + 1
+	}
+
+	carrito[producto.id] = {...producto}
+
+	pintarCarrito()
+}
+
+const pintarCarrito = () => {
+	items.innerHTML =''
+	Object.values(carrito).forEach(producto => {
+		templateCarrito.querySelector('th').textContent = producto.id
+		templateCarrito.querySelectorAll('td')[0].textContent = producto.nombre
+		templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+		templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+
+		templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+		templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+		
+		const clone = templateCarrito.cloneNode(true)
+		fragment.appendChild(clone)
+	})
+	items.appendChild(fragment)
+
+	pintarFooter()
+	accionBotones()
+}
+
+const pintarFooter = () => {
+	footer.innerHTML= ''
+	if(Object.keys(carrito).length === 0) {
+		footer.innerHTML = `
+		<th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>
+	`
+		return 
+	}
+
+	const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+	const nPrecio = Object.values(carrito).reduce((acc, { cantidad, precio}) => acc + cantidad * precio,0)
+
+	templateFooter.querySelector('td').textContent = nCantidad
+	templateFooter.querySelector('span').textContent = nPrecio
+
+	const clone = templateFooter.cloneNode(true)
+	fragment.appendChild(clone)
+	footer.appendChild(fragment)
+
+	const btnVaciarCarrito = document.getElementById('vaciar-carrito')
+	btnVaciarCarrito.addEventListener('click', () => {
+		carrito = {}
+		pintarCarrito()
+	})
+
+}
 
 
+const accionBotones = () => {
+	const botonesAgregar = document.querySelectorAll('#items .btn-info')
+	const botonesEliminar = document.querySelectorAll('#items .btn-danger')
 
+	botonesAgregar.forEach(btn => {
+		btn.addEventListener('click', () => {
+			const producto = carrito[btn.dataset.id]
+			 producto.cantidad++
+			 carrito[btn.dataset.id] = { ...producto }
+			 pintarCarrito()
+		})
+	})
 
+	botonesEliminar.forEach(btn => {
+		btn.addEventListener('click', () => {
+			const producto = carrito[btn.dataset.id]
+			producto.cantidad--
+			
+			if(producto.cantidad === 0) {
+				delete carrito[btn.dataset.id]
+			} else {
+				carrito[btn.dataset.id] = { ...producto}
+			}
+			pintarCarrito()
+		})
+	})
+	
 
+}
+
+const fetchData = () => {
+
+	let productos = {
+	  1: { nombre: "Remera BTC", precio: 15,"img": "../Pages/Img/btc.jpg" },
+	  2: { nombre: "Remera DOT", precio: 15,"img": "../Pages/Img/dot.jpg" },
+	  3: { nombre: "Remera ETH", precio: 15,"img": "../Pages/Img/eth.jpg" },
+	  4: { nombre: "Remera ADA", precio: 15,"img": "../Pages/Img/cardano.jpg" },
+	  5: { nombre: "Remera SOL", precio: 15,"img": "../Pages/Img/solana.jpg" },
+	  6: { nombre: "Remera Evolucion", precio: 15,"img": "../Pages/Img/evolution.jpg" },
+	};
+
+	pintarCards(productos)	
+}
