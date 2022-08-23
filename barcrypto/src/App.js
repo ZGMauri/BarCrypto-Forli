9b958358afece9341c0750f98106ed4cf602a8ff
ZGMauri <mauri1891.mm@gmail.com>
2022-08-23 12:24:50 -0300

    prueba react

--- barcrypto/src/App.js
+++ barcrypto/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import ColorSchemesExample from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+<><script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script><script
+  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+  crossorigin></script><script
+    src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+    crossorigin></script><script>var Alert = ReactBootstrap.Alert;</script></>
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ColorSchemesExample />
+      
+      <div>
+        <div className="App">
+          <Button className="btn btn-primary">Prueba</Button>
+        </div>
+        <h1>Probando</h1>
+      </div>
     </div>
   );
 }
--- barcrypto/src/index.js
+++ barcrypto/src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+<><script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script><script
+  src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+  crossorigin></script><script
+    src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+    crossorigin></script><script>var Alert = ReactBootstrap.Alert;</script></>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
